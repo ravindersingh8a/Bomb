71adce0cf9afec9d171f30bdbff53572517ccad7
ravindersingh8a <ravindersingh8a@gmail.com>
2021-01-19 20:36:24 +0530

    Add files via upload

---
+++ Camp.java
@@ -0,0 +1,33 @@
+package com.example.QuadTree;
+import java.util.*;
+public class Camp {
+
+    private float x;
+    private float y;
+
+    public Camp(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // getters & toString()
+
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+
+}
